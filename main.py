2bf19b1fddfdac36b71cc815b8d63575e5385a0a
paieer <paieer@users.noreply.github.com>
2025-02-20 22:22:34 +0800

    添加随机字符串生成函数

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Depends, HTTPException, Header
 import yfinance as yf
 import os
+import random
+import string
 
 # from dotenv import load_dotenv
 # # 加载 .env 文件中的环境变量
@@ -8,8 +10,16 @@ import os
 
 app = FastAPI()
 
+SESSION_PROXY= os.getenv("SESSION_PROXY", "").strip() or None
+SESSION_A= os.getenv("SESSION_A", "").strip() or None
+SESSION_B= os.getenv("SESSION_B", "").strip() or None
+
 HTTP_PROXY = os.getenv("HTTP_PROXY", "").strip() or None
 VALID_API_KEY = os.getenv("API_KEY", "default-secret-key").strip()
+
+def generate_random_string(length=8):
+            letters_and_digits = string.ascii_letters + string.digits
+            return ''.join(random.choice(letters_and_digits) for i in range(length))
 
 async def verify_api_key(api_key: str = Header(..., alias="X-API-Key")):
     if api_key != VALID_API_KEY:
@@ -22,8 +32,13 @@ async def verify_api_key(api_key: str = Header(..., alias="X-API-Key")):
 @app.get("/tickers/{symbol}", dependencies=[Depends(verify_api_key)])
 async def get_stock_info(symbol: str):
     try:
-        
-        dat = yf.Ticker(symbol,proxy=HTTP_PROXY)
+        random_string = generate_random_string()
+        if SESSION_PROXY=="TRUE":
+            Proxy = SESSION_A + random_string + SESSION_B 
+            dat = yf.Ticker(symbol,proxy=Proxy)
+        else:
+            dat = yf.Ticker(symbol,proxy=HTTP_PROXY)
+            
         info = dat.info
         
         # 处理无效的股票代码或空数据
