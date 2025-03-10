93173feb23e758fdf3dbed399eea5fa8650f73d8
paieer <paieer@users.noreply.github.com>
2025-03-11 00:36:38 +0800

    修改随机字符串生成函数，仅使用数字而不再包含字母

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def validate_date_format(date_str):
         return False
 
 def generate_random_string(length=8):
-    letters_and_digits = string.ascii_letters + string.digits
-    return ''.join(random.choice(letters_and_digits) for i in range(length))
+    # letters_and_digits = string.ascii_letters + string.digits
+    return ''.join(random.choice(string.digits) for i in range(length))
 
 
 @cached(cache=TTLCache(maxsize=500, ttl=3600*24))
